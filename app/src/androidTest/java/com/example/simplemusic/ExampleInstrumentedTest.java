208eaba9bac02dfd01c213df237390275947071b
Li, Dengke <dengke.a.li@rakuten.com>
2023-03-14 00:09:04 +0900

    Fix gradle test

--- app/src/androidTest/java/com/example/simplemusic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/simplemusic/ExampleInstrumentedTest.java
@@ -1,13 +1,14 @@
 package com.example.simplemusic;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.simplemusic", appContext.getPackageName());
     }
